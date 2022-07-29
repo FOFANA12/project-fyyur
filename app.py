b4c4e310d663d4f3e3c55fbcb8664e84129aaec6
FOFANA12 <fofbaky@gmail.com>
2022-07-29 04:05:27 +0000

    Search for artist

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def search_venues():
       })
       
   response={
-    "count": len(venues),
+    "count": len(data),
     "data": data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
@@ -392,13 +392,20 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_term = request.form['search_term']
+  artists = Artist.query.filter(Artist.name.ilike('%' + search_term + '%')).all()
+  data = []
+  
+  for artist in artists:
+      num_upcoming_shows = Show.query.filter(Show.artist_id==artist.id).filter(Show.start_time>datetime.now()).count()
+      data.append({
+        "id": artist.id,
+        "name": artist.name,
+        "num_upcoming_shows": num_upcoming_shows,
+      })
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(data),
+    "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
