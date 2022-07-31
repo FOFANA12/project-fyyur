75408f2ffb8fe834a62ed97c454ccf8ad6336554
FOFANA12 <fofbaky@gmail.com>
2022-07-31 18:18:15 +0000

    code cleanup

--- app.py
+++ app.py
@@ -32,16 +32,12 @@ app.cli.add_command(init_data)
 #----------------------------------------------------------------------------#
 
 def format_datetime(value, format='medium'):
-  #if isinstance(value, str):
     date = dateutil.parser.parse(value)
     if format == 'full':
         format="EEEE MMMM, d, y 'at' h:mma"
     elif format == 'medium':
         format="EE MM, dd, y h:mma"
     return babel.dates.format_datetime(date, format, locale='en')
-  #else:
-    #date = value
-
 app.jinja_env.filters['datetime'] = format_datetime
 
 #----------------------------------------------------------------------------#
