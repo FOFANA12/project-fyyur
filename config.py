e6ef5ae3e9de6fe77c1c0f567db324f220a3644b
FOFANA12 <fofbaky@gmail.com>
2022-08-04 09:33:01 +0000

    change DB URI

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@127.0.0.1:5432/projectfyyur'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@127.0.0.1:5432/projectfyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://hcorblbbzelurs:3afd641952c0a6f20e604cfab6924f7b2fd40d6b7c2c7f1d8ee136095d842eb7@ec2-54-85-56-210.compute-1.amazonaws.com:5432/dd70in5fgskkal'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
